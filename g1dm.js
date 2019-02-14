0f512b42de0a47cbcc50d082e5e2699e745ceaaa
Stchen Slem <stchen.sl@gmail.com>
2019-02-14 14:28:50 +0700

    Update g1dm.js

--- g1dm.js
+++ g1dm.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="2019/02/virtual.html"}
-if(x=="2"){xxx="2019/02/dedicated.html"}
-if(x=="3"){xxx="2019/02/cheap.cloud.html"}
-if(x=="4"){xxx="2019/02/big.data.html"}
-if(x=="5"){xxx="2019/02/Tableau.html"}a_to_ve[i].href="http://data.visitlinks.xyz/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="virtual.html"}
+if(x=="2"){xxx="dedicated.html"}
+if(x=="3"){xxx="cheap.cloud.html"}
+if(x=="4"){xxx="big.data.html"}
+if(x=="5"){xxx="Tableau.html"}a_to_ve[i].href="http://data.visitlinks.xyz/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
