4362de486ef2072325c8bf4ac5da1311a73d2b34
Stchen Slem <stchen.sl@gmail.com>
2020-04-20 07:40:26 +0700

    Update serv.js

--- serv.js
+++ serv.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://jumpertrick.blogspot.com/2015/03/big-data-solutions.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://kang-flasher.blogspot.com/p/bigdata.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "n";
